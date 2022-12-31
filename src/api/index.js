6512e37aeb830b9eadb4b762f1377f56cc41e69d
Sanjay-6326 <sanjaybahukhandi100@gmail.com>
2022-12-31 18:10:34 +0530

    update baseurl

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://stack-overflow-server-ydjc.onrender.com'})
+const API = axios.create({ baseURL: 'https://stack-overflow-snjy.netlify.app'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
