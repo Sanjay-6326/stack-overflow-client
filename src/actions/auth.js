cabd41b3a55d63a4ee8152009a559320497a7130
Sanjay-6326 <sanjaybahukhandi100@gmail.com>
2023-01-02 20:04:02 +0530

    update

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,7 +5,7 @@ export const signup = (authData, navigate) => async (dispatch) => {
     try {
         const { data } = await api.signUp(authData)
         dispatch({ type: 'AUTH', data })
-        dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile')) ))
+        dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile'))))
         navigate('/')
     } catch (error) {
         console.log(error)
